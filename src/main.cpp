2c3eb4b10a982d6c44576c029dbac2cb38bb688c
GeekoftheWild <thealmonddestroyer@gmail.com>
2022-01-03 17:53:42 +1100

    made code neater

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,42 @@
 #include <string>
 #include "findCommand.h"
 
+class Commands
+{
+private:
+    void Print()
+    {
+        std::cout << "CGeekL: Hello";
+    }
+    void Bye()
+    {
+        std::cout << "CGeekL: Bye";
+    }
+    void supercalifragilisticexpialidocious()
+    {
+        std::cout << "CGeekL: You discovered a secret message! Congrats! Supercalifragilisticexpialidocious!";
+    }
+public:
+    std::string input;
+    void findCommand()
+    {
+        if (input == "print")
+        
+            Print();
+        else if (input == "bye")
+        
+            Bye();
+        else if (input == "supercalifragilisticexpialidocious")
+            supercalifragilisticexpialidocious();
+    }
+};
 
 int main() {
-    std::string input;
-    while (input != "exit") {
+    Commands commands;
+    while (commands.input != "exit") {
         std::cout << "CGeekL > ";
-        std::getline(std::cin, input);
-        findCommand(input);
+        std::getline(std::cin, commands.input);
+        commands.findCommand();
     }
     return 0;
 }               
