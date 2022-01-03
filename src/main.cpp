497b59dd45311c09f49be05333c2ddbab5920e8d
GeekoftheWild <thealmonddestroyer@gmail.com>
2022-01-03 18:09:33 +1100

    deleted some stuff

--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,29 @@ class Commands
 private:
     void Print()
     {
-        std::cout << "CGeekL: Hello";
+        std::cout << "CGeekL: Hello" << std::endl;
     }
     void Bye()
     {
-        std::cout << "CGeekL: Bye";
+        std::cout << "CGeekL: Bye" << std::endl;
     }
     void supercalifragilisticexpialidocious()
     {
-        std::cout << "CGeekL: You discovered a secret message! Congrats! Supercalifragilisticexpialidocious!";
+        std::cout << "CGeekL: You discovered a secret message! Congrats! Supercalifragilisticexpialidocious!" << std::endl;
     }
 public:
     std::string input;
     void findCommand()
     {
         if (input == "print")
-        
             Print();
         else if (input == "bye")
         
             Bye();
         else if (input == "supercalifragilisticexpialidocious")
             supercalifragilisticexpialidocious();
+        else
+            system(input.c_str());
     }
 };
 
